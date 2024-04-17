3f71eb3ec352346ad5cd19cd7945b38282b9d7d0
alejop0927 <alejoperes2003@gmail.com>
2024-04-17 17:30:23 -0500

    add: logotype and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,44 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Facebook",
+            style: TextStyle(
+                fontFamily: 'Arial',
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  child: Image(
+                      image: NetworkImage(
+                          'https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png')),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
